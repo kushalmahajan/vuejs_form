262c0f709766d21db2213c57d5c9b3df682fa655
Kushal Vedant Mahajan <16890@aegonlife.com>
2018-09-19 12:06:17 +0530

    Vue material removed

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
 import wrap from '@vue/web-component-wrapper';
-
-Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
 const CustomElm = wrap(Vue, App);
 
 window.customElements.define('custom-elem', CustomElm);
-// new Vue({
-// 	render: h => h(App),
-// }).$mount('#app');
+new Vue({
+	render: h => h(App),
+}).$mount('#app');
